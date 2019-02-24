7a2c7c469da54b7c982f34a04b322afa71739b14
Miguel Rust <hava12soft@gmail.com>
2019-02-23 17:36:32 -0700

    Enabled CORS.

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const port = 3010;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
 
-app.use(function(req, res, next){
+
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
