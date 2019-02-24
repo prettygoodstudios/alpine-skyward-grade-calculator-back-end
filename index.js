401d0c58798bc4aa1c9ea43aed08d2b04ab5a15a
Miguel Rust <hava12soft@gmail.com>
2019-02-23 18:08:32 -0700

    Switched over to port 8080.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const skyward = require('skyward-rest');
 const express = require('express')
 const bodyParser = require('body-parser');  
 const app = express()
-const port = 3010;
+const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
